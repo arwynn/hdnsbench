4f6748f884ef0419ba8f2a7c46601444a8741972
Daria Zimarina <d.zimarina@ndmsystems.com>
2015-04-30 17:39:02 +0300

    open new socket for each request & stats

--- Main.hs
+++ Main.hs
@@ -5,30 +5,42 @@ import qualified Network.DNS as DNS (lookup)
 import Network.DNS hiding (lookup)
 import Control.Monad
 import Control.Concurrent.Async
+import Control.Concurrent (threadDelay)
 import Data.List (unfoldr)
+import Data.Either (lefts)
 import Data.DomainDB
 import Control.Exception as E
 
-lookupDomains :: ResolvSeed -> [(Domain,TYPE)] -> IO ()
+--lookupDomains :: ResolvSeed -> [(Domain,TYPE)] -> IO ()
 lookupDomains rs ds =
-    withResolver rs $ \resolver -> do
-        forM_ ds $ \(dn, t) -> do
+    forM ds $ \(dn, t) -> do
+        withResolver rs $ \resolver -> do
+            threadDelay 10
             E.handle err $ DNS.lookup resolver dn t
     where err :: SomeException -> IO (Either DNSError [RDATA])
-          err _ = return (Left ServerFailure)
+          err e = print e >> return (Left ServerFailure)
 
 chunks n = takeWhile (not.null) . unfoldr (Just . splitAt n)
 
 main :: IO ()
 main = do
-    ds' <- readDomains "data/alexa-top-2000-domains.txt"
-    let n  = (length ds') `div` 3
+    ds'' <- readDomains "data/alexa-top-2000-domains.txt"
+    let ds' = take 300 ds''
+        n  = (length ds') `div` 3
         ds = chunks n ds'
-        rc = ResolvConf {resolvInfo = RCHostName "8.8.8.8"
-                       , resolvTimeout = 3 * 1000 * 1000
-                       , resolvRetry = 3
+        rc = ResolvConf {resolvInfo = RCHostPort "192.168.1.1" 53
+                       , resolvTimeout = 10 * 1000 * 1000
+                       , resolvRetry = 10
                        , resolvBufsize = 4096
                        , resolvSockType = Stream}
     rs <- makeResolvSeed rc
-    mapConcurrently (lookupDomains rs) ds
+    putStrLn "Sending requests..."
+    res' <- mapConcurrently (lookupDomains rs) ds
+    putStrLn "Finished"
+    let res = concat res'
+        tot = length res
+        err = length $ lefts res
+    putStrLn "==============================="
+    putStrLn $ "Total sent: " ++ show(tot)
+    putStrLn $ "Errors: " ++ show(err)
     return ()
